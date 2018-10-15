69f3e058fc3335518e3f4a3f4c3d38a230cf7a16
6ccattleya <6ccattleya@gmail.com>
2018-10-15 18:58:15 +0000

    add set_active_route() helper

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -6,14 +6,14 @@
 
   <div class="collapse navbar-collapse" id="menu">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{route('app.home')}}">Home <span class="sr-only">(current)</span></a>
-      </li>
       <li class="nav-item">
+        <a class="nav-link {{set_active_route('app.home')}}" href="{{route('app.home')}}">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item {{set_active_route('app.about')}}">
         <a class="nav-link" href="{{route('app.about')}}">About</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Artisan</a>
+      <li class="nav-item {{set_active_route('app.home')}}">
+        <a class="nav-link " href="#">Artisan</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Planet</a>
@@ -26,8 +26,8 @@
           <a class="dropdown-item" href="https://larachat.com">Larachat</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{route('app.contacts')}}">Contacts</a>
+      <li class="nav-item {{set_active_route('app.home')}}">
+        <a class="nav-link " href="{{route('app.contacts')}}">Contacts</a>
       </li>
     </ul>
     <ul class="navbar-nav justify-content-center">
